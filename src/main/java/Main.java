7fb04f282da8fe78718de82f9b7c268c193ace33
Richard <seter74@gmail.com>
2025-06-16 12:26:10 +0200

    7th commit final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,7 +25,7 @@ import java.util.concurrent.TimeUnit;
         for (int i = 0; i < 100; i++) {
             hippodrome.move();
             watch(hippodrome);
-            TimeUnit.MILLISECONDS.sleep(5);
+            TimeUnit.MILLISECONDS.sleep(200);
         }
 
         String winnerName = hippodrome.getWinner().getName();
