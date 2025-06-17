551e10c68d3ee97fd664eea01256550d5d3507d5
Mariano Navas <772085+marianudo@users.noreply.github.com>
2025-06-17 20:53:02 +0200

    Feedback on the HorseContructorTests

--- src/test/java/HorseConstructorTests.java
+++ src/test/java/HorseConstructorTests.java
@@ -6,36 +6,33 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+// MENTOR FEEDBACK
+/*
+    See feedback on the HorseMethodsTest class
+ */
 class HorseConstructorTests {
-
-    Horse horse;
-    @AfterEach
-    void reset() {
-        horse = null;
-    }
-
     @Test
     void shouldThrowExceptionIfNameIsNull() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> horse = new Horse(null, 1.0, 1.0));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 1.0, 1.0));
         assertEquals("Name cannot be null.", exception.getMessage());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "\t", "\n"})
     void shouldThrowExceptionIfNameIsBlank(String name) {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> horse = new Horse(name, 1.0, 1.0));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Horse(name, 1.0, 1.0));
         assertEquals("Name cannot be blank.", exception.getMessage());
     }
 
     @Test
     void shouldThrowExceptionIfSpeedIsNegative() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> horse = new Horse("Name", -1.0, 1.0));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Name", -1.0, 1.0));
         assertEquals("Speed cannot be negative.", exception.getMessage());
     }
 
     @Test
     void shouldThrowExceptionIfDistanceIsNegative() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> horse = new Horse("Name", 1.0, -1.0));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Name", 1.0, -1.0));
         assertEquals("Distance cannot be negative.", exception.getMessage());
     }
 }
