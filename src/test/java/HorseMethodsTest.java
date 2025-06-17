825133c26f86c53daf68d9ae1d3dbc1042113ded
Mariano Navas <772085+marianudo@users.noreply.github.com>
2025-06-17 20:41:33 +0200

    Feedback on the Horse class test

--- src/test/java/HorseMethodsTest.java
+++ src/test/java/HorseMethodsTest.java
@@ -8,43 +8,48 @@ import static org.mockito.Mockito.mockStatic;
 
 class HorseMethodsTest {
 
-    Horse horse;
-    @AfterEach
-    void reset() {
-        horse = null;
-    }
+    // MENTOR FEEDBACK
+    /*
+    We use attributes in test classes to reuse objects in different test methods when those objects have
+    a common setup for different test methods.
+    That common setup is set in a method annotated with @BeforeEach
+    In this case we don't have such common setup. So we don't really need a class scope Horse reference.
+    We can create a local Horse reference on each test method and remove the need of setting the horse instance to null
+    This also will prevent errors in the test execution when test methods run concurrently on the same test class instance
+    (not usually the case in JUnit 5 within maven projects, but possible in other execution environments).
+     */
 
     @Test
     void getNameTest() {
         String name = "HorsesName";
-        horse = new Horse(name, 1.0, 1.0);
+        Horse horse = new Horse(name, 1.0, 1.0);
         assertEquals(name, horse.getName());
     }
 
     @Test
     void getSpeedTest() {
         double speed = 2.5;
-        horse = new Horse("name", speed, 1.0);
+        Horse horse = new Horse("name", speed, 1.0);
         assertEquals(speed, horse.getSpeed());
     }
 
     @Test
     void getDistanceTest() {
         double distance = 3.5;
-        horse = new Horse("name", 1.0, distance);
+        Horse horse = new Horse("name", 1.0, distance);
         assertEquals(distance, horse.getDistance());
     }
 
     @Test
     void getDefaultDistanceTest() {
-        horse = new Horse("name", 1.0);
+        Horse horse = new Horse("name", 1.0);
         assertEquals(0.0, horse.getDistance());
     }
 
     @Test
     void moveShouldCallGetRandomDoubleWithProperArgs() {
         try (MockedStatic<Horse> mocked = mockStatic(Horse.class)) {
-            horse = new Horse("name", 3.0, 5.0);
+            Horse horse = new Horse("name", 3.0, 5.0);
             mocked.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(0.5);
             horse.move();
             mocked.verify(() -> Horse.getRandomDouble(0.2, 0.9));
@@ -63,7 +68,7 @@ class HorseMethodsTest {
     void moveShouldUpdateDistanceCorrectly(double speed, double distance, double random, double expected) {
         try (MockedStatic<Horse> mocked = mockStatic(Horse.class)) {
             mocked.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(random);
-            horse = new Horse("name", speed, distance);
+            Horse horse = new Horse("name", speed, distance);
             horse.move();
             assertEquals(expected, horse.getDistance());
         }
